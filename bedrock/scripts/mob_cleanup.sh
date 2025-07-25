bb58c4db93489c6fb0f46f1bff947177d92f8633
mike fettis <michael.j.fettis@gmail.com>
2025-07-25 15:00:57 -0500

    feat: adding more scripts

---
+++ bedrock/scripts/mob_cleanup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# List of hostile mobs
+mobs=(
+  blaze cave_spider
+  creeper drowned
+  elder_guardian endermite
+  evoker ghast
+  guardian hoglin
+  husk magma_cube
+  phantom piglin
+  piglin_brute pillager
+  ravager shulker
+  silverfish skeleton
+  slime spider
+  stray vex
+  vindication_illager vindicator
+  warden witch
+  wither wither_skeleton
+  zoglin zombie
+  zombie_villager zombified_piglin
+)
+
+# Print header
+echo "Select a mob to kill:"
+
+# Display in two columns with numbered options
+for ((i=0; i<${#mobs[@]}; i+=2)); do
+  left="$((i+1)). ${mobs[i]}"
+  right=""
+  if [[ $((i+1)) -lt ${#mobs[@]} ]]; then
+    right="$((i+2)). ${mobs[i+1]}"
+  fi
+  printf "%-30s %s\n" "$left" "$right"
+done
+
+# Prompt for selection
+echo
+read -p "Enter the number of the mob to kill: " choice
+
+# Validate and map to mob
+index=$((choice - 1))
+if [[ $index -ge 0 && $index -lt ${#mobs[@]} ]]; then
+  selected_mob="${mobs[$index]}"
+  echo "Killing all: $selected_mob"
+  docker exec bds send-command "kill @e[type=$selected_mob]"
+else
+  echo "Invalid selection. Exiting."
+fi
---
+++ bedrock/scripts/mob_summon.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Define mob categories
+hostile_mobs=(
+  blaze cave_spider creeper drowned elder_guardian endermite evoker ghast guardian
+  hoglin husk magma_cube phantom piglin piglin_brute pillager ravager shulker
+  silverfish skeleton slime spider stray vex vindication_illager vindicator warden
+  witch wither wither_skeleton zoglin zombie zombie_villager zombified_piglin
+)
+
+peaceful_mobs=(
+  allay axolotl bat cat chicken cow donkey fox frog glow_squid horse iron_golem
+  llama mooshroom mule ocelot panda parrot pig sheep snow_golem squid strider
+  turtle villager wandering_trader wolf
+)
+
+players=(claracorn4105 lukey2894 ridingintraffic)
+
+echo "1. Choose mob category:"
+select category in "Hostile" "Not Hostile"; do
+  if [[ $category == "Hostile" ]]; then
+    mob_list=("${hostile_mobs[@]}")
+    break
+  elif [[ $category == "Not Hostile" ]]; then
+    mob_list=("${peaceful_mobs[@]}")
+    break
+  else
+    echo "Invalid selection."
+  fi
+done
+
+# List all mobs in that category
+echo
+echo "2. Choose a mob to summon:"
+for ((i=0; i<${#mob_list[@]}; i++)); do
+  printf "%3d. %s\n" $((i+1)) "${mob_list[i]}"
+done
+
+read -p "Enter the number of the mob: " mob_choice
+mob_index=$((mob_choice - 1))
+if [[ $mob_index -ge 0 && $mob_index -lt ${#mob_list[@]} ]]; then
+  mob="${mob_list[$mob_index]}"
+else
+  echo "Invalid mob choice."
+  exit 1
+fi
+
+# Number to summon
+read -p "3. How many $mob do you want to summon? " count
+if ! [[ "$count" =~ ^[0-9]+$ ]]; then
+  echo "Please enter a valid number."
+  exit 1
+fi
+
+# Choose player
+echo
+echo "4. Choose which player to summon near:"
+select target_player in "${players[@]}"; do
+  if [[ -n "$target_player" ]]; then
+    break
+  else
+    echo "Invalid player."
+  fi
+done
+
+# Run summon command loop
+echo
+echo "Summoning $count x $mob near $target_player..."
+
+for ((i=1; i<=count; i++)); do
+  docker exec bds send-command "execute as $target_player at @s run summon $mob ~2 ~ ~"
+done
