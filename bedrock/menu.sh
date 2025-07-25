1de8150fc4441b2bd81be011344c35b5684f9f37
mike fettis <michael.j.fettis@gmail.com>
2025-07-25 14:53:06 -0500

    fix: menu.sh tweak

--- bedrock/menu.sh
+++ bedrock/menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPTS_DIR="/users/home/t0rque/git/minecraft/bedrock/scripts"
+SCRIPTS_DIR="/home/t0rque/git/minecraft/bedrock/scripts"
 
 echo "Select a script to run:"
 select script in "$SCRIPTS_DIR"/*.sh; do
